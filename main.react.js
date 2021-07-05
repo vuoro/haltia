69ca87343be5415f46c74e0d97cba1ce41cc7234
Joni Korpi <me@jonikorpi.com>
2021-07-05 18:42:05 +0300

    Fix useHaltia issues

--- main.react.js
+++ main.react.js
@@ -1,10 +1,9 @@
 import { useState, useMemo, useEffect } from "react";
 
-export { Map, Set } from "./main.js";
-
 export const useHaltia = (mapOrSet) => {
   const [, setState] = useState(mapOrSet);
-  const subscription = useMemo(() => mapOrSet.subscribe(setState), [mapOrSet]);
-  useEffect(() => () => subscription.unsubscribe(setState), [subscription]);
+  const identity = useMemo(() => () => setState((v) => v + 1), []);
+  const subscription = useMemo(() => mapOrSet.subscribe(identity), [mapOrSet, identity]);
+  useEffect(() => () => subscription.unsubscribe(identity), [subscription, identity]);
   return subscription;
 };
